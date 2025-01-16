465304841845b0a64b6a4e135b69bb56dfc81f6f
nghiahd147 <qn500787@gmail.com>
2025-01-16 15:43:29 +0700

    Load Project

---
+++ src/components/sections/resize.button.tsx
@@ -0,0 +1,21 @@
+import { CSSProperties } from "react";
+
+interface IProps {
+    btnText?: string,
+    btnIcons?: React.ReactNode,
+    btnStyle?: CSSProperties,
+    onClick?: () => void
+}
+
+const ResizeButton = (props: IProps) => {
+    const { btnText, btnIcons, btnStyle, onClick } = props;
+
+    return (
+        <button onClick={onClick} className="resize-button" style={btnStyle}>
+            <span style={{ textTransform: "uppercase" }}>{btnText}</span>
+            <>{btnIcons}</>
+        </button>
+    )
+}
+
+export default ResizeButton;
---
+++ src/helpers/data.ts
@@ -0,0 +1,173 @@
+import protocolImg from '../assets/project/protocol.png';
+import ecomerceImg from '../assets/project/webthươngmạiđiệntử.png'
+
+export const APP_DATA = {
+    YOUTUBE_URL: "https://www.youtube.com/@nghiakaedehara",
+    TIKTOK_URL: "https://www.tiktok.com/@wibu_380",
+    // UDEMY_URL: "https://www.udemy.com/user/eric-7039/",
+    FACEBOOK_URL: "https://www.facebook.com/cuchuoi.votinh.355"
+}
+
+export const EXPERIENCES = [
+    {
+        id: 1,
+        title: {
+            vi: "Dự án tự làm",
+            en: "Self Employed"
+        },
+        company: {
+            vi: "Xây dựng website protocol",
+            en: "Building a website protocol"
+        },
+        duration: {
+            vi: "Ngày 10 Tháng 1 2025 - Ngày 15 Tháng 1 2025",
+            en: "January 10, 2025 - January 15, 2025"
+        }
+    },
+    {
+        id: 2,
+        title: {
+            vi: "Dự án tự làm",
+            en: "Self Employed"
+        },
+
+        company: {
+            vi: "Xây dựng website thương mại điện tử",
+            en: "Build e-commerce website"
+        },
+        duration: {
+            vi: "Tháng 9/2024 - Tháng 10/2024",
+            en: "September 2024 - October 2024"
+        }
+    }
+]
+
+export const PROJECTS = [
+    {
+        id: 1,
+        imgPath: ecomerceImg,
+        title: "Xây dựng website thương mại điện tử",
+        description: "Đây là website sản phẩm đầu tay của mình liên quan đến thư viện ReactJs mà mình đã học được",
+        githubLink: "https://github.com/nghiahd147/Website-Ecomerce",
+        demoLink: "https://website-ecomerce.vercel.app/"
+    },
+    {
+        id: 2,
+        imgPath: protocolImg,
+        title: "Xây dựng website Protocol",
+        description: "Chính là website này đây, project này giúp mình học từ làm việc Reactjs Javascript sang Typescript",
+        githubLink: "",
+        demoLink: ""
+    }
+
+]
+
+export const SKILLS_DATA = [
+    'HTML',
+    'CSS',
+    'Javascript',
+    'Typescript',
+    'React',
+    'Next JS',
+    'Tailwind',
+    'MongoDB',
+    'Git',
+    'Bootstrap',
+  ]
+  
+  // Choose your skills from below. Make sure it's in the same format and spelled correctly.
+  // Couldn't find the required skills? Raise an issue on github at https://github.com/hhhrrrttt222111/developer-portfolio/issues/new
+  
+  
+  // AVAILABLE SKILLS
+  
+  /* 
+    HTML
+    CSS
+    JS 
+    'MySQL',
+    'PostgreSQL',
+    'AWS',
+    'Docker',
+    'Go',
+    'Figma',
+    'Firebase',
+    'MaterialUI',
+    'Nginx',
+    'Strapi'
+    React
+    Next JS
+    Nuxt JS
+    Node JS
+    Vue
+    Angular
+    Docker
+    Photoshop
+    Illustrator
+    Svelte
+    GCP
+    Azure
+    Fastify
+    Haxe
+    Ionic
+    Markdown
+    Microsoft Office
+    Picsart
+    Sketch
+    Unity
+    WolframAlpha
+    Adobe XD
+    After Effects
+    Bootstrap
+    Bulma
+    CapacitorJs
+    Coffeescript
+    MemSQL
+    C
+    C++
+    C#
+    Python
+    Java
+    Julia
+    Matlab
+    Swift
+    Ruby
+    Kotlin
+    Go
+    PHP
+    Flutter
+    Dart
+    Typescript
+    Swift
+    Git
+    Figma
+    Canva
+    Ubuntu
+    Bootstrap
+    MongoDB
+    Tailwind
+    ViteJS
+    VuetifyJS
+    MySQL
+    PostgreSQL
+    AWS
+    Firebase
+    Blender
+    Premiere Pro
+    Adobe Audition
+    Deno
+    Django
+    Gimp
+    Graphql
+    Lightroom
+    MaterialUI
+    Nginx
+    Numpy
+    OpenCV
+    Pytorch
+    Selenium
+    Strapi
+    Tensorflow
+    Webex
+    Wordpress
+  */
